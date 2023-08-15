b786b9919840920cf801dafefb7d2a8d849f3c2b
Raja Nazirul <rajanazirul93@gmail.com>
2023-08-16 00:28:04 +0800

    change title

--- cvat-ui/src/components/signing-common/signing-layout.tsx
+++ cvat-ui/src/components/signing-common/signing-layout.tsx
@@ -81,8 +81,8 @@ function SignInLayout(props: SignInLayoutComponentProps): JSX.Element {
                 <Content>
                     <Row justify='center' align='middle' style={{ height: '100%' }}>
                         <Col {...titleSizes} className='cvat-signing-title'>
-                            <Title>Open Data</Title>
-                            <Title>Annotation Platform</Title>
+                            <Title>Asset Management</Title>
+                            <Title>With AI-Driven Processing</Title>
                         </Col>
                         {children}
                     </Row>
